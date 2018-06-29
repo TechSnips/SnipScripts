227e024aaaa0980f84746527038e6ff7de936d8c
Adam Bertram <adbertram@gmail.com>
2018-06-29 12:36:02 -0500

    Adam Bertram scripts

--- How To Run Remote Commands on Hyper-V Virtual Machines with PowerShell Direct/demo.ps1
+++ How To Run Remote Commands on Hyper-V Virtual Machines with PowerShell Direct/demo.ps1
@@ -9,11 +9,14 @@
 
 	Snip suggestions:
 		N/A
+
+    Notes:
+        I am currently working from a machine outside of the Hyper-V host
 	
 #>
 
 #region Hyper-V host is remote so we'll use "regular" remoting to get into it
-$hyperVHost = '23.96.10.116'
+$hyperVHost = '40.76.212.105'
 $hyperVHostCred = Get-Credential
 Enter-PSSession -ComputerName $hyperVHost -Credential $hyperVHostCred
 
@@ -21,18 +24,27 @@ Enter-PSSession -ComputerName $hyperVHost -Credential $hyperVHostCred
 Get-Vm
 #endregion
 
-#region Typical PowerShell Remoting
-$vmName = 'SRV1'
-$vmCred = Get-Credential
+#region Typical PowerShell Remoting --failure
+## PowerShell remoting works great when it works but you've got dependencies like DNS, IP, authentication, etc
+$vmName = 'LABDC'
+$vmCred = Get-Credential -UserName 'LabUser' -Message 'user'
 Invoke-Command -ComputerName $vmName -ScriptBlock { hostname } -Credential $vmCred
+
+## Now let's try from the Hyper-V host itself
 #endregion
 
-#region PowerShell Direct
+#region Disable the NIC on the VM (from the Hyper-V host) This kills our PowerShell remoting session
+Invoke-Command -ComputerName $vmName -ScriptBlock {Get-NetAdapter | Disable-NetAdapter} -Credential $vmCred
+ping labdc
+#endregion
+
+#region PowerShell Direct --from my computer remoted into they Hyper-V host
 Invoke-Command -VMName $vmName -ScriptBlock { hostname } -Credential $vmCred
 
 ## Find the VM guid
-$vm = Get-Vm -Name SRV1
-$vm.Guid
+$vm = Get-Vm -Name LABDC
+$vmId = $vm.Id.ToString()
+$vmId
 
-Invoke-Command -VMName $vm.Guid -ScriptBlock { hostname } -Credential $vmCred
+Invoke-Command -VMID $vmId -ScriptBlock { hostname } -Credential $vmCred
 #endregion
