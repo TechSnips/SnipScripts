5ce3006d2a439717a482a1a520a782837c93d46f
Adam Bertram <adbertram@gmail.com>
2018-07-03 16:39:06 -0500

    scripts!

---
+++ How To Set And Remove The Read Only Attribute To Files With PowerShell/DemoSetup1.ps1
@@ -0,0 +1,49 @@
+﻿#############################################################
+#  Available at: https://github.com/TechSnips/SnipScripts   #
+#############################################################
+
+$folderName = 'ReadOnlyDemo'
+
+cd c:\
+
+Remove-Item C:\$folderName\ -Force -Recurse
+
+New-Item -Path c:\$folderName -ItemType Directory -Force
+New-Item -Path c:\$folderName\TestFile.txt -ItemType File -Force
+
+explorer C:\$folderName\
+
+$i = 0
+
+While ($i -le 9) {
+    New-Item -Path "C:\$folderName\Folder-$i" -ItemType Directory -Force
+    $j = 0
+
+    while ($j -le 9) {
+        New-Item -Path "C:\$folderName\Folder-$i\File-$j.txt" -Force
+        $j++
+    }
+    $i++
+}
+
+
+Set-ItemProperty -Path C:\$folderName\Folder-0\File-3.txt -Name IsReadOnly -Value $true
+Set-ItemProperty -Path C:\$folderName\Folder-0\File-7.txt -Name IsReadOnly -Value $true
+Set-ItemProperty -Path C:\$folderName\Folder-0\File-9.txt -Name IsReadOnly -Value $true
+Set-ItemProperty -Path C:\$folderName\Folder-1\File-1.txt -Name IsReadOnly -Value $true
+Set-ItemProperty -Path C:\$folderName\Folder-1\File-3.txt -Name IsReadOnly -Value $true
+Set-ItemProperty -Path C:\$folderName\Folder-1\File-4.txt -Name IsReadOnly -Value $true
+Set-ItemProperty -Path C:\$folderName\Folder-1\File-5.txt -Name IsReadOnly -Value $true
+Set-ItemProperty -Path C:\$folderName\Folder-2\File-2.txt -Name IsReadOnly -Value $true
+Set-ItemProperty -Path C:\$folderName\Folder-2\File-3.txt -Name IsReadOnly -Value $true
+Set-ItemProperty -Path C:\$folderName\Folder-2\File-4.txt -Name IsReadOnly -Value $true
+Set-ItemProperty -Path C:\$folderName\Folder-3\File-3.txt -Name IsReadOnly -Value $true
+Set-ItemProperty -Path C:\$folderName\Folder-3\File-5.txt -Name IsReadOnly -Value $true
+Set-ItemProperty -Path C:\$folderName\Folder-7\File-1.txt -Name IsReadOnly -Value $true
+Set-ItemProperty -Path C:\$folderName\Folder-7\File-5.txt -Name IsReadOnly -Value $true
+Set-ItemProperty -Path C:\$folderName\Folder-7\File-7.txt -Name IsReadOnly -Value $true
+
+cd C:\$folderName
+
+Get-ChildItem C:\$folderName -Recurse | where IsReadOnly -eq $true | select FullName
+
---
+++ How To Set And Remove The Read Only Attribute To Files With PowerShell/ReadOnlyDemo.ps1
@@ -0,0 +1,47 @@
+﻿#############################################################
+#  Available at: https://github.com/TechSnips/SnipScripts   #
+#############################################################
+
+$folderName = 'ReadOnlyDemo'
+
+Get-ChildItem -Path C:\$folderName\TestFile.txt
+
+Get-ChildItem -Path C:\$folderName\TestFile.txt | Select *
+
+#region Method 1
+
+$source = Get-ChildItem -Path C:\$folderName\TestFile.txt
+
+Set-ItemProperty -Path $source -Name IsReadOnly -Value $true
+
+Get-ChildItem -Path C:\$folderName\TestFile.txt | Select *
+
+#endregion
+
+
+#region Method 2
+
+$source = Get-ChildItem -Path C:\$folderName\TestFile.txt
+
+$source.IsReadOnly
+
+$source.IsReadOnly = $false
+
+Get-ChildItem -Path C:\$folderName\TestFile.txt | Select *
+
+#endregion
+
+
+#region Recursive
+
+Get-ChildItem -Path C:\$folderName\ -Recurse | Where IsReadOnly -EQ $true
+
+$source = Get-ChildItem -Path C:\$folderName\ -Recurse | Where IsReadOnly -EQ $true
+
+Set-ItemProperty -Path $source.fullName -Name IsReadOnly -Value $false
+
+$source | Set-ItemProperty -Name IsReadOnly -Value $false
+
+Get-ChildItem -Path C:\$folderName\ -Recurse | Where IsReadOnly -EQ $true
+
+#endregion
