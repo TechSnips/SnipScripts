adc6a97985795efb4fcbd0ca7e1500454242e007
Adam Bertram <adbertram@gmail.com>
2018-07-18 15:27:55 -0500

    new scripts

---
+++ How to Transfer Files via Secure Copy Protocol (SCP) with WinSCP/Prerequisites.ps1
@@ -0,0 +1,8 @@
+﻿
+#####################################################################
+# Prerequisites:                                                    #
+#                                                                   #
+# 1. Latest version of WinSCP: https://winscp.net/eng/download.php  #
+# 2. Another computer running an SSH server                         #
+#                                                                   #
+#####################################################################
---
+++ How to Transfer Files via Secure Copy Protocol (SCP) with WinSCP/ScriptTest.ps1
@@ -0,0 +1,2 @@
+﻿
+& "C:\Program Files (x86)\WinSCP\WinSCP.exe" /log="C:\log\WinSCP.log" /ini=nul /script="C:\Users\matt\Documents\SCPTest.txt"
