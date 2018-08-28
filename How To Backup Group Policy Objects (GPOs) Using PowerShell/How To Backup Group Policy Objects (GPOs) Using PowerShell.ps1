90f83e2dc05e4e38cbc368f2214e4f3c2b18abed
Adam Bertram <adbertram@gmail.com>
2018-08-28 13:29:18 -0500

    AD schtuff

---
+++ How To Backup Group Policy Objects (GPOs) Using PowerShell/How To Backup Group Policy Objects (GPOs) Using PowerShell.ps1
@@ -0,0 +1,83 @@
+﻿Import-Module grouppolicy 
+
+#region Simple Backup
+
+Get-GPO -All
+
+Get-GPO -All | Backup-GPO -Path 'c:\GPO_Backup'
+
+#endregion
+
+
+#region Named Directory
+
+$GPO = Get-GPO -Name HR
+
+$BackupDestination = 'c:\GPO_Backup\' + $GPO.DisplayName
+
+New-Item -Path $BackupDestination -ItemType directory
+
+$GPO | Backup-GPO -Path $BackupDestination
+
+#endregion
+
+#region Modification Time
+
+$GPO = Get-GPO -Name HR
+
+$pattern = '[^a-zA-Z0-9\s]'
+
+$ModificationTime = $GPO.ModificationTime -replace $pattern,''
+
+$BackupDestination = 'c:\GPO_Backup\' + $GPO.DisplayName + '\' + $ModificationTime + '\'
+
+New-Item -Path $BackupDestination -ItemType directory | Out-Null
+
+$GPO | Backup-GPO -Path $BackupDestination
+
+#endregion
+
+#region DisplayName
+
+$GPO = Get-GPO -Name "Add Shortcut!¯\_(ツ)_/¯"
+
+$pattern = '[^a-zA-Z0-9\s]'
+
+$DisplayName = $GPO.DisplayName -replace $pattern,''
+
+$ModificationTime = $GPO.ModificationTime -replace $pattern,''
+
+$BackupDestination = 'c:\GPO_Backup\' + $DisplayName + '\' + $ModificationTime + '\'
+
+New-Item -Path $BackupDestination -ItemType directory | Out-Null
+
+$GPO | Backup-GPO -Path $BackupDestination
+
+#endregion
+
+#region Backup all the things
+
+$AllGPOs = Get-GPO -All
+
+foreach ($GPO in $AllGPOs) {
+
+  $pattern = '[^a-zA-Z0-9\s]'
+
+  $DisplayName = $GPO.DisplayName -replace $pattern,''
+
+  $ModificationTime = $GPO.ModificationTime -replace $pattern,''
+
+  $BackupDestination = 'c:\GPO_Backup\' + $DisplayName + '\' + $ModificationTime + '\'
+
+
+  if (-Not (Test-Path $BackupDestination)) {
+
+      New-Item -Path $BackupDestination -ItemType directory | Out-Null
+
+      Backup-GPO -Name $GPO.DisplayName -Path $BackupDestination
+
+      }
+
+  } 
+
+#endregion
---
+++ How To Restore Active Directory Objects From The Recycle Bin With PowerShell/How To Restore Active Directory Objects From The Recycle Bin With PowerShell.ps1
@@ -0,0 +1,10 @@
+﻿
+Get-ADObject -Filter *
+
+Get-ADObject -Filter {Deleted -eq $True} -IncludeDeletedObjects
+
+Get-ADObject -Filter {Deleted -eq $True} -IncludeDeletedObjects | where {$_.ObjectClass -eq 'user'}
+
+Get-ADObject -Filter {Deleted -eq $True} -IncludeDeletedObjects | where {$_.ObjectClass -eq 'user'} | Out-GridView
+
+Get-ADObject -Filter {Deleted -eq $True} -IncludeDeletedObjects | where {$_.ObjectClass -eq 'user'} | Out-GridView -PassThru | Restore-ADObject
