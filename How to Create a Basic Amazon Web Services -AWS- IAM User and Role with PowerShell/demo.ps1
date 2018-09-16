579aa96c8a825bfa91abc10cb4b2a2982d4356b1
Adam Bertram <adbertram@gmail.com>
2018-09-16 13:42:52 -0500

    Update demo.ps1

--- How to Create a Basic Amazon Web Services -AWS- IAM User and Role with PowerShell/demo.ps1
+++ How to Create a Basic Amazon Web Services -AWS- IAM User and Role with PowerShell/demo.ps1
@@ -6,17 +6,17 @@
 Set-AWSCredential -AccessKey '<root user access key>' -SecretKey '<root user secret key>'
 
 ## Create a no-frills IAM user
-$iamUserName = 'TechSnips'
+$iamUserName = 'TechSnipsDemo'
 New-IAMUser -UserName $iamUserName
 
 ## Find the managed policy called AdministratorAccess' ARN that has access to all AWS services
 $policyArn = (Get-IAMPolicies | where {$_.PolicyName -eq 'AdministratorAccess'}).Arn
 
 ## Attach the AdministratorAccess policy to the user we just created
-Register-IAMUserPolicy -PolicyArn $policyArn -UserName AutomateBoringStuff
+Register-IAMUserPolicy -PolicyArn $policyArn -UserName $iamUserName
 
 ## Create a new IAM access and secret key for the user
-$key = New-IAMAccessKey -UserName AutomateBoringStuff
+$key = New-IAMAccessKey -UserName $iamUserName
 
 ## Authenticate to your AWS account using the newly created access key and secret access key. This time, storing it
 ## as the default keys so it will keep even across PowerShell sessions
