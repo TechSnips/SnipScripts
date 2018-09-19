80108bc121a4c5cc26e58b5df48e7f1eb58b19d9
Adam Bertram <adbertram@gmail.com>
2018-09-19 10:27:43 -0500

    Update New-CustomEC2Instance.ps1

--- How To Create An Amazon Web Services -AWS- EC2 Instance In PowerShell From The Ground Up/New-CustomEC2Instance.ps1
+++ How To Create An Amazon Web Services -AWS- EC2 Instance In PowerShell From The Ground Up/New-CustomEC2Instance.ps1
@@ -29,14 +29,15 @@ function New-CustomEC2Instance {
 		[string]$InstanceType
 	)
 
-	## Create the VPC using a user-defined CIDR block (network)
+	## Create the VPC using a user-defined CIDR block (network) only if it doesn't already exist
 	if (-not ($vpc = Get-EC2Vpc | where CidrBlock -eq $VpcCidrBlock)) {
 		Write-Verbose -Message "Creating new VPC with CIDR block of [$($VpcCidrBlock)]..."
 		$vpc = New-EC2Vpc -CidrBlock $VpcCidrBlock
 	} else {
 		Write-Verbose -Message "A VPC with the CIDR block [$($VpcCidrBlock)] has already been created."
 	}
-
+	
+	## Enable DNS support on the VPC only if we've chosen to do so
 	if ($EnableDnsSupport.IsPresent) {
 		Write-Verbose -Message "Enabling DNS support on VPC ID [$($vpc.VpcId)]..."
 		## Add DNS support to the VPC
@@ -47,7 +48,8 @@ function New-CustomEC2Instance {
 		Edit-EC2VpcAttribute -VpcId $vpc.VpcId -EnableDnsHostnames $true
 	}
 
-	## Create the internet gateway that will act as a router for Internet traffic for our VPC
+	## Create the internet gateway that will act as a router for Internet traffic for our VPC only if one isn't already
+	## attached to the VPC.
 	if (-not ($gw = Get-EC2InternetGateway | where { $_.Attachments.VpcId -eq $vpc.VpcId })) {
 		Write-Verbose -Message 'Creating new internet gateway...'
 		$gw = New-EC2InternetGateway
@@ -59,7 +61,7 @@ function New-CustomEC2Instance {
 		Write-Verbose -Message "An internet gateway is already attached to VPC ID [$($vpc.VpcId)]."
 	}
 
-	## Create a new route table on our VPC so we can add a default route to it
+	## Create a new route table on our VPC so we can add a default route to it only if it doeesn't already exist
 	if (-not ($rt = Get-EC2RouteTable -Filter @{ Name='vpc-id'; Value=$vpc.VpcId })) {
 		Write-Verbose -Message "Creating route table for VPC ID [$($vpc.VpcId)]..."
 		$rt = New-EC2RouteTable -VpcId $vpc.VpcId
@@ -67,6 +69,7 @@ function New-CustomEC2Instance {
 		Write-Verbose -Message "Route table already exists for VPC ID [$($vpc.VpcId)]."
 	}
 	
+	## Create a default route only if one doesn't already exist
 	if (-not ($rt.Routes | where {$_.DestinationCidrBlock -eq '0.0.0.0/0'})) {
 		## Create a default route (default gateway) to the route table pointing to the internet gateway to complete
 		## the configuration to enable EC2 instances attached to this VPC to get to the Internet
@@ -76,7 +79,7 @@ function New-CustomEC2Instance {
 		Write-Verbose -Message "A default route has already been created for route table ID [$($rt.RouteTableId)]."
 	}
 
-	## Create the subnet and add it to the route table
+	## Create the subnet and add it to the route table only if there's not a subnet created already
 	if (-not ($sn = Get-EC2Subnet -Filter @{ Name='vpc-id'; Value=$vpc.VpcId })) {
 		Write-Verbose -Message "Creating and registering a subnet with CIDR block [$($SubnetCidrBlock)] with VPC ID [$($vpc.VpcId)]..."
 		$sn = New-EC2Subnet -VpcId $vpc.VpcId -CidrBlock $SubnetCidrBlock -AvailabilityZone $SubnetAvailabilityZone
@@ -94,6 +97,8 @@ function New-CustomEC2Instance {
 			throw "Unrecognized operating system: [$_]"
 		}
 	}
+	
+	## Ensure an AMI is found. Otherwise, return an error
 	if (-not ($ami = Get-EC2ImageByName -Name $imageName)) {
 		Write-Error -Message "No AMI found with the image name of [$($imageName)]."
 	} else {
