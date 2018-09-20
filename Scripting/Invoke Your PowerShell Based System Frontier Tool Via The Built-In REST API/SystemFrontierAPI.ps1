dd0b18b10193b00ee5284360d740053f6a6c492c
Adam Bertram <adbertram@gmail.com>
2018-09-20 16:35:33 -0500

    script reorg

---
+++ Scripting/Invoke Your PowerShell Based System Frontier Tool Via The Built-In REST API/SystemFrontierAPI.ps1
@@ -0,0 +1,29 @@
+﻿$SystemFrontierURI = 'http://systemfrontier.techsnips.io:8080/api/job'
+
+$Params = @{
+    Target = 'workstation1;workstation2;workstation3'
+    Depth  = 2
+}
+
+$Tool = @{
+    CustomToolID = "f7f47639-db94-4312-a6ef-fdfcc0e688c1"
+    TargetClass  = "Computer"
+    Parameters   = $Params
+}
+
+$JSON = ConvertTo-Json @(,$Tool)
+
+$RestParams = @{
+    Method                = 'Post'
+    Uri                   = $SystemFrontierURI
+    Body                  = $JSON
+    ContentType           = 'application/json'
+    UseDefaultCredentials = $true
+}
+$Job = Invoke-RestMethod @RestParams
+
+$Result = Invoke-RestMethod -Method Get -Uri "$SystemFrontierURI/$Job" -UseDefaultCredentials
+
+foreach ($Task in $Result.Tasks) {
+    $Task | Select TargetName, Result
+}
---
+++ Scripting/Turning An Existing PowerShell Script Into A Web Based Tool With System Frontier/Find-LocalPST.ps1
@@ -0,0 +1,16 @@
+﻿param (
+    [String] $ComputerName,
+    [Int] $Depth = 2
+)
+
+if (Test-Connection -ComputerName $ComputerName -Count 2 -Quiet) {
+    $Items = Get-ChildItem -Path "\\$ComputerName\C$" -Filter '*.pst' -Depth $Depth -ErrorAction SilentlyContinue
+    
+    if ($Items) {
+        $Items.FullName
+    } else {
+        Write-Output'No PST found'
+    }
+} else {
+    Write-Output "Not responding to ping"
+}
