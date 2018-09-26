a09350a1e4861af5cb2f7682d2382cc7ecfcf804
Adam Bertram <adbertram@gmail.com>
2018-09-26 10:02:29 -0500

    Update Invoke-UserLogoff.ps1

--- How to Log a Windows User Off Remotely with PowerShell/Invoke-UserLogoff.ps1
+++ How to Log a Windows User Off Remotely with PowerShell/Invoke-UserLogoff.ps1
@@ -1,8 +1,34 @@
+<#
+	.SYNOPSIS
+		This script connects to a remote computer, checks for any user sessions and, if found, logs all sessions off.
+
+	.EXAMPLE
+		PS> .\Invoke-UserLogoff.ps1 -ComputerName FOO
+
+		This example connects to the computer name of FOO and logs off all users.
+
+	.EXAMPLE
+		PS> .\Invoke-UserLogoff.ps1 -ComputerName FOO,BAR,BAZ
+
+		This example connects to all computers and logs off all users on each computer.
+
+	.EXAMPLE
+		PS> .\Invoke-UserLogoff.ps1 -ComputerName FOO -UserName adam
+
+		This example connects to the computer name of FOO, looks for users logged in matching username of Adam and logs him off.
+
+	.PARAMETER ComputerName
+		A mandatory string collection parameter representing one or more computer names separated by a comma to log off users from.
+
+	.PARAMETER UserName
+		An optional string parameter representing a single user to log off.
+
+#>
 [OutputType('void')]
 [CmdletBinding(SupportsShouldProcess)]
 param
 (
-	[Parameter()]
+	[Parameter(Mandatory)]
 	[ValidateNotNullOrEmpty()]
 	[string[]]$ComputerName,
 
@@ -13,16 +39,9 @@ param
 
 $ErrorActionPreference = 'Stop'
 
-if (-not $PSBoundParameters.ContainsKey('ComputerName')) {
-	$ComputerName = 'localhost'
-}
-
 foreach ($c in $ComputerName) {
 	## Find the user's session ID
-	$compArgs = $null
-	if ($PSBoundParameters.ContainsKey('ComputerName')) {
-		$compArgs = "/server:$c"	
-	}
+	$compArgs = "/server:$c"
 	$whereFilter = { '*' }
 	if ($PSBoundParameters.ContainsKey('UserName')) {
 		$whereFilter = [scriptblock]::Create("`$_ -match '$UserName'")
