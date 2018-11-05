c4bc0dcabc5fc64bd4aa8885cefde9916b2ea616
Adam Bertram <adbertram@gmail.com>
2018-11-05 08:12:39 -0600

    new scripts!

---
+++ Applying And Removing A Hybrid Use Benefit License For An Existing Azure VM With PowerShell/nmqio1vwT3yiioh7fqKD_Applying And Removing A Hybrid Use Benefit License For An Existing Azure VM With PowerShell.ps1
@@ -0,0 +1,20 @@
+﻿
+Connect-AzureRmAccount
+
+$VMTarget = Get-AzureRmVM | Select-Object Name, ResourceGroupName, LicenseType | Out-GridView -PassThru
+
+function Set-AzureRMVMHUBLicense ($VMTarget) {
+    $vm = Get-AzureRmVM -ResourceGroup $VMTarget.ResourceGroupName -Name $VMTarget.Name
+    $vm.LicenseType = "Windows_Server"
+    Update-AzureRmVM -ResourceGroupName $VMTarget.ResourceGroupName -VM $vm
+}
+
+function Remove-AzureRMVMHUBLicense ($VMTarget) {
+    $vm = Get-AzureRmVM -ResourceGroup $VMTarget.ResourceGroupName -Name $VMTarget.Name
+    $vm.LicenseType = "None"
+    Update-AzureRmVM -ResourceGroupName $VMTarget.ResourceGroupName -VM $vm
+}
+
+Set-AzureRMVMHUBLicense -VMTarget $VMTarget
+
+Remove-AzureRMVMHUBLicense -VMTarget $VMTarget
