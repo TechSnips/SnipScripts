b9afe80365e77dd194f0344b69ee4d348faa9df1
Adam Bertram <adbertram@gmail.com>
2019-01-07 09:28:30 -0600

    new stuff!

---
+++ How To Install The Azure Az PowerShell Module And Authenticate To Azure/I3KozRHrRzOL7mINV1r5_Install-Az.ps1
@@ -0,0 +1,16 @@
+﻿#Check to see if the module is already installed
+Get-Module -Name Az* -ListAvailable
+
+#Install the AzureRM module from the PowerShell Gallery
+Install-Module -Name Az -AllowClobber
+
+#Connect and authenticate to your Azure subscription
+Connect-AzAccount
+
+#Verify success by listing virtual machines
+Get-AzVM -Location "canadacentral"
+
+#Add the PowerShell Gallery as a trusted repository
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Get-PSRepository
+
---
+++ How To Migrate From The AzureRM PowerShell Module To The Azure Az Module/0Z0RXFeGRQqRkTTDQeT6_Migrate-to-Az.ps1
@@ -0,0 +1,27 @@
+﻿#Check to see which version of AzureRM is installed
+Get-Module -Name AzureRm -ListAvailable
+
+#Uninstall the AzureRM module
+Uninstall-Module -Name AzureRm
+
+Get-Module -Name AzureRm* -ListAvailable
+
+foreach ($AzModule in (Get-Module -Name AzureRm* -ListAvailable).Name | Get-Unique)
+{
+    Uninstall-Module $AzModule -Verbose
+}
+
+#Install the AzureRM module from the PowerShell Gallery
+Install-Module -Name Az -AllowClobber
+
+#Connect and authenticate to your Azure subscription
+Connect-AzAccount
+
+#Verify success by listing virtual machines
+Get-AzVM -Location "canadacentral"
+
+#Enable AzureRM compatibility aliases
+Enable-AzureRmAlias -Scope CurrentUser
+
+#Verify success by listing virtual machines using AzureRM alias
+Get-AzureRmVM -Location "canadacentral"
